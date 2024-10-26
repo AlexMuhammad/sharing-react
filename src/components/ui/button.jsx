e4e141ae269711345964e10e8bf853ed575932b7
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-10-26 09:04:37 +0700

    feat: solution

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ name, bgColor = "bg-yellow-500", ...props }) => {
   return (
     <button
-      className={` py-1 px-3 rounded-md text-white ${bgColor}`}
+      className={` py-1 duration-300 transition-colors px-3 rounded-md text-white ${bgColor}`}
       {...props}
     >
       {name}
--- src/pages/time-tracking-page.jsx
+++ src/pages/time-tracking-page.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../components/ui/button";
+import { formatTime } from "../utils";
 
 export const TimeTrackingPage = () => {
+  const [tasks, setTasks] = useState([]);
+  const [activeTask, setActiveTask] = useState({
+    name: "",
+    timeSpent: 0,
+    startTime: null,
+    isRunning: false,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (activeTask.isRunning) {
+        setActiveTask((prev) => ({
+          ...prev,
+          timeSpent: prev.timeSpent + 1,
+        }));
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [activeTask.isRunning]);
+
+  const startHandler = () => {
+    if (activeTask.name.trim()) {
+      setActiveTask((prev) => ({
+        ...prev,
+        startTime: new Date(),
+        isRunning: true,
+      }));
+    }
+  };
+
+  const pauseHandler = () => {
+    setActiveTask((prev) => ({
+      ...prev,
+      isRunning: false,
+    }));
+  };
+
+  const saveHandler = () => {
+    if (activeTask.timeSpent > 0) {
+      const newTask = {
+        id: Date.now(),
+        name: activeTask.name,
+        startTime: activeTask.startTime,
+        timeSpent: activeTask.timeSpent,
+        endTime: new Date(),
+      };
+      setTasks((prev) => [...prev, newTask]);
+      setActiveTask({
+        name: "",
+        timeSpent: 0,
+        startTime: null,
+        isRunning: false,
+      });
+    }
+  };
+
   return (
     <div className="w-full max-w-2xl mx-auto">
       <div className="bg-slate-100 p-3">
@@ -16,26 +74,53 @@ export const TimeTrackingPage = () => {
             id="activity"
             placeholder="What do we do today?"
             className="flex-1 p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 border"
+            onChange={(e) =>
+              setActiveTask((prev) => ({ ...prev, name: e.target.value }))
+            }
+            value={activeTask.name}
           />
         </div>
         <div className="flex items-center justify-between mt-8">
-          <span className="font-semibold text-2xl font-mono">00:00:01</span>
+          <span className="font-semibold text-2xl font-mono">
+            {formatTime(activeTask.timeSpent)}
+          </span>
           <div className="flex items-center gap-3">
-            <Button name="Start" bgColor="bg-green-500" type="submit" />
-            <Button name="Stop" />
-            <Button name="Save" bgColor="bg-red-500" />
+            {!activeTask.isRunning ? (
+              <Button
+                name="Start"
+                bgColor="bg-green-500"
+                type="submit"
+                onClick={startHandler}
+              />
+            ) : (
+              <Button name="Stop" onClick={pauseHandler} />
+            )}
+            <Button name="Save" bgColor="bg-red-500" onClick={saveHandler} />
           </div>
         </div>
       </div>
       <div className="bg-green-100 p-3 space-y-2">
-        <div className="bg-white space-y-1 p-3 rounded-md flex justify-between">
+        {tasks.length > 0 ? (
+          tasks.map((task) => (
+            <div className="bg-white space-y-1 p-3 rounded-md flex justify-between">
+              <div>
+                <h3 className="font-semibold">{task.name}</h3>
+                <p className="text-sm text-gray-500 font-light">
+                  {task.timeSpent}
+                </p>
+                <p className="text-sm text-gray-500">
+                  {task.startTime.toLocaleTimeString()} -
+                  {task.endTime.toLocaleTimeString()}
+                </p>
+              </div>
+              <Button name="Delete" bgColor="bg-red-500" />
+            </div>
+          ))
+        ) : (
           <div>
-            <h3 className="font-semibold">Task Name</h3>
-            <p className="text-sm text-gray-500 font-light">00:00:01</p>
-            <p className="text-sm text-gray-500">9:59:31 PM - 9:59:33 PM</p>
+            <p className="text-center">No Data</p>
           </div>
-          <Button name="Delete" bgColor="bg-red-500" />
-        </div>
+        )}
       </div>
     </div>
   );
